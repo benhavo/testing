e7d2ff342021434df2c3423ba3ec8f16554a11a3
Ben Havrilesko <bh3553@gmail.com>
2014-10-24 12:03:27 -0400

    First Commit

---
+++ phpinfo.php
@@ -0,0 +1,16 @@
+<?php 
+//phpinfo(); 
+$ben = 2+5;
+echo $ben;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html>
